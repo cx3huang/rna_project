fix masking

dataset.py
@@ -16,13 +16,13 @@ class RNADataset(Dataset):
         df_2A3 = df_2A3.iloc[split].reset_index(drop=True)
         df_DMS = df_DMS.iloc[split].reset_index(drop=True)
 
-        b = (df_2A3['SN_filter'].values > 0.5) & (df_DMS['SN_filter'].values > 0.5)
-        df_2A3 = df_2A3.loc[b].reset_index(drop=True)
-        df_DMS = df_DMS.loc[b].reset_index(drop=True)
+        # b = (df_2A3['SN_filter'].values > 0.5) & (df_DMS['SN_filter'].values > 0.5)
+        # df_2A3 = df_2A3.loc[b].reset_index(drop=True)
+        # df_DMS = df_DMS.loc[b].reset_index(drop=True)
 
         self.seq = df_2A3['sequence'].values
         
-        '''TODO: check if this implementation correctly returns sequences'''
+
         self.react_2A3 = df_2A3[[c for c in df_2A3.columns if \
                                  'reactivity_0' in c]].values
         self.react_DMS = df_DMS[[c for c in df_DMS.columns if \
@@ -32,8 +32,8 @@ class RNADataset(Dataset):
         self.react_err_DMS = df_DMS[[c for c in df_DMS.columns if \
                                 'reactivity_error_0' in c]].values
         
-        self.snr_2A3 = df_2A3['signal_to_noise'].values
-        self.snr_DMS = df_DMS['signal_to_noise'].values
+        # self.snr_2A3 = df_2A3['signal_to_noise'].values
+        # self.snr_DMS = df_DMS['signal_to_noise'].values
 
 
     def __len__(self):
@@ -42,16 +42,24 @@ class RNADataset(Dataset):
     def __getitem__(self, idx):
         seq = self.seq[idx]
         seq = np.array([self.bp_mapping[s] for s in seq])
-        seq = np.pad(seq,(0,self.Lmax-len(seq)))
 
         mask = torch.zeros(self.Lmax, dtype=torch.bool)
         mask[:len(seq)] = True
         
-        reactivity = torch.from_numpy(np.stack([self.react_2A3[idx],
-                                           self.react_DMS[idx]],-1))
+        seq = np.pad(seq,(0,self.Lmax-len(seq)))
+
+        s2A3 = self.react_2A3[idx]
+        s2A3[s2A3 < 0] = 0
+
+        sDMS = self.react_DMS[idx]
+        sDMS[sDMS < 0] = 0
+
+        # reactivity = torch.from_numpy(np.stack([self.react_2A3[idx],
+        #                                    self.react_DMS[idx]],-1))
+        reactivity = torch.from_numpy(np.stack([s2A3, sDMS],-1))
         reactivity_err = torch.from_numpy(np.stack([self.react_err_2A3[idx],
                                                self.react_err_DMS[idx]],-1))
-        snr = torch.FloatTensor([self.snr_2A3[idx], self.snr_DMS[idx]])
+        # snr = torch.FloatTensor([self.snr_2A3[idx], self.snr_DMS[idx]])
         
         # return {'seq':torch.from_numpy(seq), \
         #         'reactivity': reactivity, \